46926b2f5c8156132f68484c90ab4df2483973f7
Drizzt <Drizzt@Drizzt-PC>
2015-05-20 17:07:15 +0800

    修改参数名称，改为小写

--- template/${basepackage_dir}/service/${className}Service.java
+++ template/${basepackage_dir}/service/${className}Service.java
@@ -19,29 +19,29 @@ import ${basepackage}.model.${className}Example;
 public class ${className}Service{
 	
 	@Resource
-	private ${className}Mapper ${className}Mapper;
+	private ${className}Mapper ${classNameLower}Mapper;
 	
 	public void add${className}(${className} ${classNameLower}) {
-		${className}Mapper.insertSelective(${classNameLower});
+		${classNameLower}Mapper.insertSelective(${classNameLower});
 	}
 	
 	public void del${className}By${pkColumnName}(${pkSimpleJavaType} ${pkColumnNameLower}) {
-		${className}Mapper.deleteByPrimaryKey(${pkColumnNameLower});
+		${classNameLower}Mapper.deleteByPrimaryKey(${pkColumnNameLower});
 	}
 	
 	public void update${className}(${className} ${classNameLower}) {
-		${className}Mapper.updateByPrimaryKeySelective(${classNameLower});
+		${classNameLower}Mapper.updateByPrimaryKeySelective(${classNameLower});
 	}
 	
 	public ${className} get${className}By${pkColumnName}(${pkSimpleJavaType} ${pkColumnNameLower}) {
-		return ${className}Mapper.selectByPrimaryKey(${pkColumnNameLower});
+		return ${classNameLower}Mapper.selectByPrimaryKey(${pkColumnNameLower});
 	}
 	
 	public List<${className}> getPage${className}(${className}Example ${classNameLower}Example) {
-		return ${className}Mapper.selectPage(${classNameLower}Example);
+		return ${classNameLower}Mapper.selectPage(${classNameLower}Example);
 	}
 	
 	public int count${className}(${className}Example ${classNameLower}Example) {
-		return ${className}Mapper.countByExample(${classNameLower}Example);
+		return ${classNameLower}Mapper.countByExample(${classNameLower}Example);
 	}
 }
